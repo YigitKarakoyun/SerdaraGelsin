bf1ea8a645df1407821dfafe1bd536434751094c
Yiğit KARAKOYUN <kodsever@outlook.com>
2019-12-07 20:54:51 +0300

    Otobus

---
+++ Otobus/Otobus/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otobus
+{
+    public partial class Form1 : Form
+    {
+        public Form2 frm2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            baslangıc:
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Dispose();
+                    goto baslangıc;
+                }
+            }
+            int satirSayisi = 0;
+            bool arkaBesli = false;
+            int KoltukNo = 0;
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                satirSayisi = 14;
+                arkaBesli = true;
+            }
+            else
+            {
+                satirSayisi = 12;
+                arkaBesli = false;
+            }
+
+
+            for (int i = 0; i < satirSayisi; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j == 2 && (arkaBesli == true && i != satirSayisi -1)
+                        || (j == 2 && arkaBesli == false)
+                        )
+                    {
+                        continue;
+                    }
+
+                    if (i==6 && j> 2)
+                    {
+                        continue;
+                    }
+
+
+                    Button btn = new Button();
+                    btn.Width = 30;
+                    btn.Height = 30;
+                    btn.Top = 50 + (i * 30);
+                    btn.Left = 100 + (j * 30);
+                    btn.Text = (++KoltukNo).ToString();
+                    btn.Click += btn_MyClick;
+                    this.Controls.Add(btn);
+                }
+            }
+
+        }
+
+        private void btn_MyClick(object sender, EventArgs e)
+        {
+            Button tiklananButon = (Button)sender;
+            frm2 = new Form2();
+            frm2.koltukNo = tiklananButon.Text;
+            frm2.Show();
+        }
+    }
+}
---
+++ sinavHazirlik/sinavHazirlik/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinavHazirlik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int satir = 12;
+        int sutun = 5;
+        Form2 frm2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBox combo = new ComboBox();
+            combo.Location = new Point(350, 20);
+            combo.Items.Add("Solaris");
+            combo.Items.Add("Ikarus");
+            combo.SelectedIndexChanged += MyCombo_SelectedIndexChange;
+            this.Controls.Add(combo);
+
+            Button btn = new Button();
+            btn.Height = 30;
+            btn.Width = 70;
+            btn.Text = "Giriş Yap";
+            btn.Location = new Point(350, 70);
+            this.Controls.Add(btn);
+        }
+
+        private void MyCombo_SelectedIndexChange(object sender, EventArgs e)
+        {
+            //ButonDispose();
+            ComboBox cmb = (ComboBox)sender;
+            if (cmb.SelectedIndex == 0)
+            {
+                MessageBox.Show(cmb.SelectedItem.ToString());
+                satir = 12;
+                sutun = 5; 
+            }
+            else
+            {
+                MessageBox.Show(cmb.SelectedItem.ToString());
+                satir = 14;
+                sutun = 4; 
+            }
+            KoltukDuzeni(satir, sutun);
+        }
+
+        private void KoltukDuzeni(int boy, int en)
+        {
+            ButonDispose();
+            int koltukNo = 1;
+            for (int i = 0; i < boy; i++)//satir
+            {
+                for (int j = 0; j < en; j++) // sutun
+                {
+                    Button btn = new Button();
+                    btn.Height = 30;
+                    btn.Width = 30;
+                    btn.Name = "btn" + koltukNo;
+                    btn.Tag = "koltuk";
+                    btn.Text = koltukNo.ToString();
+                    btn.Location = new Point(j*30, i*30);
+                    btn.Click += Btn_Click;
+                    this.Controls.Add(btn);
+                    koltukNo++;
+                }
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button tiklananButon = (Button)sender;
+            frm2 = new Form2(tiklananButon);
+            frm2.ShowDialog();
+        }
+
+        private void ButonDispose()
+        {
+            baslangic:
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    string s = item.Tag?.ToString();
+                    if (s == "koltuk")
+                    {
+                        item.Dispose();
+                        goto baslangic;
+                    }
+                }
+            }
+        }
+    }
+}
